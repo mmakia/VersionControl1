70313f775536156bd99f855e28919ca74fc5a147
Mohammed Makia <mmakia@outlook.com>
2015-09-09 13:52:54 -0400

    InitialCommit

---
+++ VersionControl1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(VersionControl1.Startup))]
+namespace VersionControl1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
